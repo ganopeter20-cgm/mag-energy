f19bdb7d4fc3863bb0a1a6f25355ad441646b543
ganopeter20-cgm <ganopeter20@gmail.com>
2026-08-30 11:06:39 +0300

    Add robots and sitemap

---
+++ mag-energy-v2/app/sitemap.js
@@ -0,0 +1,20 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://mag-energy.vercel.app",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://mag-energy.vercel.app/about",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://mag-energy.vercel.app/services",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://mag-energy.vercel.app/contact",
+      lastModified: new Date(),
+    },
+  ];
+}
